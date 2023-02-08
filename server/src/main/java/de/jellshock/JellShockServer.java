47fd17facdcf8aa3b22a07af7492770c1fdb894d
EinfachBeez <einfachbeez@t-online.de>
2023-02-08 13:46:53 +0100

    Add server module, socketio dependency

---
+++ server/src/main/java/de/jellshock/JellShockServer.java
@@ -0,0 +1,10 @@
+package de.jellshock;
+
+/**
+ * @author EinfachBeez | https://github.com/EinfachBeez
+ */
+public class JellShockServer {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
