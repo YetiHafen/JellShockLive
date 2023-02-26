3d3393aa88a294a27416c108d9c8dddccd7989bc
EinfachBeez <einfachbeez@t-online.de>
2023-02-26 23:26:08 +0100

    Replace ShapeRenderer with Pixmap

--- core/src/de/jellshock/game/world/World.java
+++ core/src/de/jellshock/game/world/World.java
@@ -1,13 +1,15 @@
 package de.jellshock.game.world;
 
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.Disposable;
 import lombok.Getter;
 
+import java.io.File;
 import java.util.Random;
 
 @Getter
@@ -15,7 +17,6 @@ public class World implements Disposable {
 
     private final Pixmap pixmap;
     private Texture texture;
-    private final ShapeRenderer shapeRenderer;
     private final Random random;
 
     private final int width;
@@ -24,8 +25,6 @@ public class World implements Disposable {
     private final float frequency;
     private final int amplitude;
 
-    float z;
-
     public World(int width, int height, int waveLength, int amplitude) {
         this.width = width;
         this.height = height;
@@ -33,65 +32,52 @@ public class World implements Disposable {
         this.frequency = 1.0f / waveLength;
         this.amplitude = amplitude;
         pixmap = new Pixmap(width, height, Pixmap.Format.RGB888);
-        shapeRenderer = new ShapeRenderer();
         random = new Random();
     }
 
-    int i;
-    int y;
-
     public void generateWorld() {
-
-        // Random value while using LCG
-
-
-        texture = new Texture(pixmap);
-    }
-
-    public void renderWorld(SpriteBatch batch) {
-        /*batch.draw(texture, 0, 0, width, height);*/
-
-        float randomState = random.nextFloat() * 0xFFFFFFFF - 1;
-        float y = height / 2f;
-
         float a = random.nextFloat();
         float b = random.nextFloat();
 
-        for (int i = 0; i < width; i++) {
-            if (!(i % waveLength == 0)) {
-                y = height / 2f + interpolate(a, b, (i % waveLength) * 1.0f / (float) waveLength) * amplitude;
+        for (int x = 0; x < width; x++) {
+            float y;
+            if (!(x % waveLength == 0)) {
+                y = height / 2f + interpolate(a, b, (x % waveLength) * 1.0f / (float) waveLength) * amplitude;
             } else {
                 a = b;
                 b = random.nextFloat();
                 y = height / 2f + (a * amplitude);
             }
-            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-            shapeRenderer.rect(i, y, 1, 1);
-            shapeRenderer.end();
-            /*pixmap.setColor(Color.BLACK);
-            pixmap.drawPixel(i, (int) y);*/
+            pixmap.setColor(Color.RED);
+            pixmap.drawPixel(x, (int) y);
+
+            for (int j = 0; j < height; j++) {
+                if (j < y) {
+                    pixmap.setColor(Color.LIGHT_GRAY);
+                    pixmap.drawPixel(x, j);
+                }
+            }
         }
 
-
+        PixmapIO.writePNG(new FileHandle(new File("test.png")), pixmap);
+        texture = new Texture(pixmap);
     }
 
-    private float random() {
-        z = (1664525 * z + 1) % 4294967296f;
-        return z / 4294967296f - 0.5f;
+    public void renderWorld(SpriteBatch batch) {
+        batch.draw(texture, 0, 0, width, height);
     }
 
-    private float interpolate(float leftPosition, float rightPosition, float relativeDistance) {
-        // calc the weight of the two texture values (leftPosition, rightPosition)
+    private float interpolate(float aPosition, float bPosition, float relativeDistance) {
+        // calc the weight of the two texture values (aPosition, bPosition)
         float scaledValue = relativeDistance * (float) Math.PI;
         // weight of the two texture values of the relative position
         float weight = (1 - (float) Math.cos(scaledValue)) * 0.5f;
-        return leftPosition * (1 - weight) + rightPosition * weight;
+        return aPosition * (1 - weight) + bPosition * weight;
     }
 
     @Override
     public void dispose() {
         texture.dispose();
         pixmap.dispose();
-        shapeRenderer.dispose();
     }
 }
