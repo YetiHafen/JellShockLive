6a3c3c9f353c515bf91967e38c8c3a89d0c8322b
Felix Burkhardt <56166282+EinfachBeez@users.noreply.github.com>
2023-03-15 14:56:54 +0100

    Add logo

--- core/src/de/jellshock/game/screen/menu/AbstractMenuScreen.java
+++ core/src/de/jellshock/game/screen/menu/AbstractMenuScreen.java
@@ -8,16 +8,19 @@ import de.jellshock.game.screen.AbstractScreen;
 public abstract class AbstractMenuScreen extends AbstractScreen {
 
     protected Texture backgroundTexture;
+    protected Texture logoTexture;
     protected SpriteBatch spriteBatch;
 
     public AbstractMenuScreen() {
         backgroundTexture = new Texture("background/background.png");
+        logoTexture = new Texture("jellshock.png");
         spriteBatch = new SpriteBatch();
     }
 
     @Override
     public void render(float delta) {
         spriteBatch.draw(backgroundTexture, 0 ,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        spriteBatch.draw(logoTexture, Gdx.graphics.getWidth() / 2F, 100);
     }
 
     @Override
